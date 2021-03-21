b010e598b29a3a6636c7a4a2ab12355df7739459
CharumruthaPrakash <charumrutha.p@gmail.com>
2021-03-21 21:04:03 +0530

    My resume

---
+++ resources/views/index.blade.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Charumrutha Prakash</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Charumrutha Prakash</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{ asset('assets/img/profile.jpg') }}" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                <!-- <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li> -->
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Charumrutha
+                        <span class="text-primary">Prakash</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Chennai · Tamil Nadu, India 600033 · (729) 987-8989 ·
+                        <a href="mailto:charumrutha.p@gmail.com" class="text-lowercase">charumrutha.p@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">To work in a challenging environment where my skills and knowledge are utilized for the achievement of the objectives of the organization.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/charu-prakash/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/CharumruthaPrakash"><i class="fab fa-github"></i></a>
+                        <!--<a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>-->
+                        <a class="social-icon" href="https://www.facebook.com/charumrutha"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Development Engineer</h3>
+                            <div class="subheading mb-3">Megamind Computing Solutions Private Limited</div>
+                            <p>Bring us a problem and we’ll run it through our ideation process to understand users, goals, strategy, and a plan for success.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">November 2020 - March 2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Developer</h3>
+                            <div class="subheading mb-3">Shippingchimps Technologies Private Limited</div>
+                            <p>A cloud based e-Commerce operations platform that helps retailers automatically sync their orders, customers, logistics informations in one place to supercharge customer-experience, reduce operational expenses with data-driven decisions</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">March 2020 - September 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Developer</h3>
+                            <div class="subheading mb-3">Blackpond Global Digital Services Private Limited</div>
+                            <p>Capitalize on the strategic, technological & financial benefits of open source software. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Feburary 2019 - Feburary 2020</span></div>
+                    </div>
+                    <!-- <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Web Design Intern</h3>
+                            <div class="subheading mb-3">Shout! Media Productions</div>
+                            <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+                    </div> -->
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SSN College of Engineering, Affliated to Anna University</h3>
+                            <div class="subheading mb-3">Masters of Engineering</div>
+                            <div>Computer Science and Engineering</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">August 2016 - June 2018</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Bharathidasan University</h3>
+                            <div class="subheading mb-3">Bachelor of Technology</div>
+                            <div>Computer Science and Engineering</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">August 2016 - June 2018</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Shri B.S. Mootha Girls' Senior Secondary School</h3>
+                            <div class="subheading mb-3">Class 12</div>
+                            <div>Science with Computer Science</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2011 - March 2012</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5" title="HTML5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt" title="CSS3"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js" title="Javascript & jQuery"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php" title="PHP"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-laravel" title="Laravel other frameworks like Yii2 & Phalcon"></i></li>                        
+                        <li class="list-inline-item"><i class="fab fa-angular" title="Angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react" title="React"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-python" title="Python"></i></li>
+                        <li class="list-inline-item"><i class="fas fa-database" title="Database Technologies: MySQL, MS SQL Server, Workbench"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-github" title="GIT/GitHub"></i></li>        <li class="list-inline-item"><i class="fab fa-bootstrap" title="Bootstrap"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-docker" title="Docker"></i></li>
+                        <li class="list-inline-item"><i class="fas fa-tools" title="Other tools: XAMPP, LaTex, MS Office"></i></li>
+
+                        <!-- <li class="list-inline-item"><i class="fab fa-node-js"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-sass"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-less"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-wordpress"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-gulp"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-grunt"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-npm"></i></li> -->
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Follow Software Development Life Cycle Process
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Responsive Design and Functionality Development
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>Apart from being a software geek, I enjoy most of my time being outdoors. I spend time in photography, playing basketball and badminton, bird watching and exploring new places. I love to cook, sing and dance.</p>
+                    <p class="mb-0">When forced indoors, I read books and follow a number of romantic comedy and fantasy genre movies,  and television shows, I am much passionated in performing yoga, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <!-- <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Google Analytics Certified Developer
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Mobile Web Specialist - Google Certification
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            2
+                            <sup>nd</sup>
+                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - James Buchanan High School - Hackathon 2006
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            3
+                            <sup>rd</sup>
+                            Place - James Buchanan High School - Hackathon 2005
+                        </li>
+                    </ul>
+                </div>
+            </section> -->
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{ asset('js/scripts.js') }}"></script>
+    </body>
+</html>
